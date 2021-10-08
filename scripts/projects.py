78ba700a353c8d0c93a3d00b1bf4b33d4b46ab4c
Mahmoud Jahanshahi <mahmoud.jahanshahi@gmail.com>
2021-10-07 21:16:29 -0400

    fixed jq readable json format

--- scripts/projects.py
+++ scripts/projects.py
@@ -1,16 +1,17 @@
 #getting projects with more than 100k commits from mongodb
-import pymongo
-import bson
+from pymongo import MongoClient
+from bson.json_util import dumps
      
-client = pymongo.MongoClient("mongodb://da1.eecs.utk.edu/")
-db = client ['WoC']
+client = MongoClient("mongodb://da1.eecs.utk.edu/")
+db = client['WoC']
 
 for v in ["S","T"]:
     coll_name = "P_metadata." + v
-    path = "../data/projects." + v
+    path = "../data/projects_" + v + ".json"
     coll = db[coll_name]
     dataset = coll.find( { "NumCommits": { "$gt": 100000 } } , no_cursor_timeout=True )
+    list_dataset = list(dataset)
+    json_data = dumps(list_dataset)
     with open(path, 'w') as f:
-        for data in dataset:
-            print >> f, data
+        f.write(json_data)
     dataset.close()
