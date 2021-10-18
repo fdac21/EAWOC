e3005f3ce3bf8a79e79939d2679bf50575d3a41b
Mahmoud Jahanshahi <mahmoud.jahanshahi@gmail.com>
2021-10-18 17:35:10 -0400

    sorted files naming bug fix

--- scripts/temporal.sh
+++ scripts/temporal.sh
@@ -4,16 +4,17 @@
 cat ../data/projects.csv |
 cut -d\; -f1 |
 while read p; do
+    path="../data/sorted/$(echo $p | sed 's|/|_|g').sorted";
     echo $p |
     ~/lookup/getValues -f P2c |
     cut -d\; -f2 |
     ~/lookup/getValues c2dat |
     cut -d\; -f1,2 |
     ~/lookup/lsort 20G -t\; -n -k2,2 |
-    sed "s|^|$p;|" > ../data/$p.sorted;
-    head -10000 < ../data/$p.sorted |
+    sed "s|^|$p;|" > $path;
+    head -10000 < $path |
     sed 's|$|;1|' ;
-    tail -10000 < ../data/$p.sorted |
+    tail -10000 < $path |
     sed 's|$|;2|' ;
 done |
 gzip > ../data/temporal.csv.gz;
